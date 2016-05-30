b48639d0f1ab65df001836e73a8c16012a09d069
yichangsin <ycs318@naver.com>
2016-05-30 15:19:58 +0900

    충돌을 해결함

--- src/tutorial/Helloworld.java
+++ src/tutorial/Helloworld.java
@@ -9,11 +9,8 @@ public class Helloworld {
 		
 		System.out.println("hotfix 브랜치 생성 후 이동하여 작업 중...");
 		
-<<<<<<< HEAD
 		System.out.println("hotfix 브랜치와 master브랜치를 합병함...");
-=======
 		System.out.println("hotfix 브랜치에서 작업 중...");
->>>>>>> hotfix
 
 	}
 
